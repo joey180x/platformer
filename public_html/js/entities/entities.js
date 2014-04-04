ab9b4328ad405ab5762a8d3c895fb2b219a1b42c
joey180x <joey180x@icloud.com>
2014-04-04 08:27:21 -0700

    initial commit
    first commit of melon js platform game

---
+++ public_html/js/entities/entities.js
@@ -0,0 +1,24 @@
+game.PLayerEntity = me.ObjectEntity.extend({
+    init: function(x, y, settings) {
+        settings.image = "player1-spritesheet";
+        settings.spritewidth = "72";
+        settings.spriteheight = "97";
+        this.parent(x, y, settings);
+        
+        this.setVelocity(5, 20);
+    },
+    
+    update: function() {
+        if(me.input.isKeyPressed("right")) {
+            this.vel.x += this.accel.x = me.timer.tick;
+        }
+        else {
+           this.vel.x = 0;
+        }
+        
+        this.updateMovement();
+        return true;
+
+    }
+    
+});
